beb9188171c04bccec4466743365c8610c0f9d04
semious <semious@gmail.com>
2025-06-17 18:08:30 +0800

    update

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Rigidbody2D rb;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal"), rb.velocity.y);
     }
 }
