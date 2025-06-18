4fb20d8aed884ef173684e556a60f2fbc3aeb0c7
semious <semious@gmail.com>
2025-06-18 18:53:19 +0800

    update

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,82 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public Rigidbody2D rb;
+    private Animator anim;
+    private Rigidbody2D rb;
+
+    [SerializeField] private float moveSpeed = 3.5f;
+    [SerializeField] private float jumpForce = 8f;
+    private float xInput;
+
+    [SerializeField] private bool facingRight = true;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    private void Start()
+    {
+    }
 
     private void Update()
     {
-        rb.velocity = new Vector2(Input.GetAxis("Horizontal"), rb.velocity.y);
+        HandleInput();
+        HandleMovement();
+        HandleAnimations();
+        HandleFlip();
+    }
+
+    private void HandleAnimations()
+    {
+        bool isMoving = rb.velocity.x != 0;
+
+        anim.SetBool("isMoving", isMoving);
+    }
+
+    private void HandleInput()
+    {
+        xInput = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void HandleMovement()
+    {
+
+        Vector2 vector2 = new(xInput * moveSpeed, rb.velocity.y);
+        rb.velocity = vector2;
+    }
+
+    private void Jump()
+    {
+
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    private void HandleFlip()
+    {
+        if (rb.velocity.x > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (rb.velocity.x < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        transform.Rotate(0, 180 ,0);
+        facingRight = !facingRight;
     }
 }
